e326dfc0119e4b940a77e546d79d31480fc4aa04
Kevin Limperos <klimperos@gmail.com>
2013-01-31 16:48:13 -0500

    Added open source license details

--- org/softwareartisans/util/workgroup/retry/ImmediateCounting.java
+++ org/softwareartisans/util/workgroup/retry/ImmediateCounting.java
@@ -1,5 +1,29 @@
 package org.softwareartisans.util.workgroup.retry;
 
+/*
+ Copyright (c) 2013 Software Artisans, LLC
+ Author: Kevin Limperos, klimperos@softwareartisans.org
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,7 +32,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ImmediateCounting implements Retrier {
-	// TODO: Change these to ArrayLists: Map is overkill
+	// TODO: Refactor to ArrayLists
 	private static final Map<Integer, Integer> taskRetryCounts = new ConcurrentHashMap<Integer, Integer>();
 	private static final Map<Integer, Integer> groupRetryCounts = new ConcurrentHashMap<Integer, Integer>();
 	private int globalRetryCount;
